a7b1770809a77603a792ccf9acd880d0893b7795
Anton Lundberg <antlundb@kth.se>
2020-03-02 11:08:07 +0100

    added hi/lo-pass filter functions and fixed librosa Fortran array bug in plot_audio_f0

--- data/util.py
+++ data/util.py
@@ -6,6 +6,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import librosa
 from librosa.display import specshow
+from ddsp import spectral_ops
+from scipy import signal
+
+def pass_filter(audio, audio_rate, cutoff, btype="high", order=5):
+    '''
+    High/low pass filter.
+    '''
+    f_n = 0.5 * audio_rate
+    cutoff_normal = cutoff / f_n
+    b, a = signal.butter(order, cutoff_normal, btype=btype, analog=False)
+    return signal.filtfilt(b, a, audio)
 
 def plot_data_dict(data, save_path=None):
     '''
@@ -26,17 +37,26 @@ def plot_data_dict(data, save_path=None):
     if save_path is not None:
         plt.savefig(save_path)
 
-def plot_audio_f0(audio, audio_rate, f0, f0_rate, fmax=8192, n_fft=8192, n_mels=1024):
+def plot_audio_f0(audio, audio_rate, f0, f0_rate, fmax=24000, n_fft=8192, n_mels=1024, ymax=24000, title=None):
+    '''
+    Plot Mel spectrogram and f0 signal.
+    '''
+    audio = np.asfortranarray(audio)
     S = librosa.feature.melspectrogram(y=audio, sr=audio_rate, n_fft=n_fft, n_mels=n_mels, fmax=fmax)
     S_dB = librosa.power_to_db(S, ref=np.max)
-    ax = specshow(S_dB, x_axis='time', y_axis='mel', sr=audio_rate, fmax=fmax)
+    ax = specshow(S_dB, x_axis="time", y_axis="mel", sr=audio_rate, fmax=fmax)
     secs = len(audio)/audio_rate
     t_f0 = np.arange(0., secs, 1./f0_rate)
     f0_h, = ax.plot(t_f0, f0, "--")
-    ax.set_ylim((0, 5*np.max(f0)))
+    #ymax = 100*np.max(f0)
+    ax.set_ylim((0, ymax))
+    step = 100 if ymax < 2000 else 1000
+    ax.yaxis.set_ticks(np.arange(0., ymax, step))
     ax.set_xlabel("time [s]")
     ax.set_ylabel("frequency [Hz]")
     ax.legend([f0_h], ["f0"], loc="upper right")
+    if title:
+        ax.set_title(title)
     plt.tight_layout()
 
 def get_timestamp():
