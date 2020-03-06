1022e73e30477d0bb0b70baf543bfa87599b5320
Anton Lundberg <antlundb@kth.se>
2020-03-06 16:15:42 +0100

    added possibility to set device (specific gpu) and to use model_id to specify which model to train

--- trainer.py
+++ trainer.py
@@ -7,12 +7,17 @@ from absl import app, logging, flags
 from ddsp.training.train_util import Trainer, write_gin_config, get_strategy
 
 from data_provider import TFRecordProvider
-from model_builder import ModelBuilder
+from model_builder import ModelBuilder, get_model_builder_from_id
 from models.losses import MelSpectralLoss, AdaptiveMelSpectralLoss, TimeFreqResMelSpectralLoss
 
 FLAGS = flags.FLAGS
+flags.DEFINE_string("model_id", None, "ID of model, as defined in ModelConfigs in model_builder.py.")
 flags.DEFINE_string("checkpoint_dir", None, "Directory to store checkpoints.")
 flags.DEFINE_string("data_dir", None, "Directory of training data (TFRecords).")
+flags.DEFINE_integer("batch_size", 32, "Batch size.")
+flags.DEFINE_integer("steps_per_summary", 1, "Training steps per summary.")
+flags.DEFINE_integer("steps_per_save", 10, "Training steps per checkpoint save.")
+flags.DEFINE_list("devices", None, "Training devices.")
 
 def train(data_provider,
           trainer,
@@ -85,49 +90,56 @@ def train(data_provider,
 
 
 def main(argv):
-    if FLAGS.checkpoint_dir is None:
-        ValueError("checkpoint_dir must be set.")
-    if FLAGS.data_dir is None:
-        ValueError("data_dir must be set.")
+    if FLAGS.model_id is not None:
+        logging.info("Building model '%s'..." % FLAGS.model_id)
+        model_builder = get_model_builder_from_id(FLAGS.model_id)
+        FLAGS.checkpoint_dir = model_builder.checkpoint_dir
+        FLAGS.data_dir = model_builder.data_dir
+        model = model_builder.build()
 
+    logging.info("Loading data from '%s'..." % FLAGS.data_dir)
+    data_provider = TFRecordProvider(FLAGS.data_dir)
+    
+    if FLAGS.model_id is None:
+        if FLAGS.checkpoint_dir is None:
+            raise ValueError("checkpoint_dir must be set.")
+        if FLAGS.data_dir is None:
+            raise ValueError("data_dir must be set.")
+        n_cylinders = 4.
+        #losses = [MelSpectralLoss(sample_rate=data_provider.audio_rate, n_bands=2)]
+        #losses = [AdaptiveMelSpectralLoss(sample_rate=data_provider.audio_rate, n_bands=8)]
+        losses = [TimeFreqResMelSpectralLoss(sample_rate=data_provider.audio_rate,
+                                            time_res=1/data_provider.input_rate)]
+        model = ModelBuilder(
+            model_type="f0_rnn_fc_hpn_decoder",
+            audio_rate=data_provider.audio_rate,
+            input_rate=data_provider.input_rate,
+            window_secs=data_provider.example_secs,
+            f0_denom=n_cylinders,
+            checkpoint_dir=FLAGS.checkpoint_dir,
+            losses=losses,
+            feature_domain="time"
+        ).build()
+
+    logging.info("Building trainer...")
     summary_dir = os.path.join(FLAGS.checkpoint_dir, "summaries", "train")
     if not os.path.exists(summary_dir):
         os.makedirs(summary_dir)
-
-    logging.info("Loading data...")
-    data_provider = TFRecordProvider(FLAGS.data_dir)
-
-    logging.info("Building model...")
-    n_cylinders = 4.
-    #losses = [MelSpectralLoss(sample_rate=data_provider.audio_rate, n_bands=2)]
-    #losses = [AdaptiveMelSpectralLoss(sample_rate=data_provider.audio_rate, n_bands=8)]
-    losses = [TimeFreqResMelSpectralLoss(sample_rate=data_provider.audio_rate,
-                                         time_res=1/data_provider.input_rate)]
-    model = ModelBuilder(
-        model_type="f0_rnn_fc_hpn_decoder",
-        audio_rate=data_provider.audio_rate,
-        input_rate=data_provider.input_rate,
-        window_secs=data_provider.example_secs,
-        f0_denom=n_cylinders,
-        checkpoint_dir=FLAGS.checkpoint_dir,
-        losses=losses,
-        feature_domain="time"
-    ).build()
-
-    logging.info("Building trainer...")
-    gpus = ["/physical_device:GPU:0", "/physical_device:GPU:1"]
-    strategy = get_strategy(gpus=gpus)
+    strategy = tf.distribute.MirroredStrategy(devices=FLAGS.devices)
     trainer = Trainer(model, strategy)
 
     logging.info("Initializing training...")
     while True:
-        #try:
-        train(data_provider, trainer, batch_size=1,
-                                          steps_per_summary=1,
-                                          steps_per_save=10,
+        try:
+            train(data_provider, trainer, batch_size=FLAGS.batch_size,
+                                          steps_per_summary=FLAGS.steps_per_summary,
+                                          steps_per_save=FLAGS.steps_per_save,
                                           model_dir=FLAGS.checkpoint_dir)
-        #except:
-        #    logging.info("An error ocurred, reinitializing training from last checkpoint...")
+        except KeyboardInterrupt:
+            logging.info("Registered control-C event, stopping.")
+            break
+        except:
+            logging.info("An error ocurred, reinitializing training from last checkpoint...")
 
 if __name__ == "__main__":
     app.run(main)
