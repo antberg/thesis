b4869692cb489cbefe64f8274eb578152f717312
Anton Lundberg <antlundb@kth.se>
2020-03-18 12:42:43 +0100

    added OscF0Preprocessor

--- models/preprocessors.py
+++ models/preprocessors.py
@@ -3,6 +3,11 @@ from ddsp.training.preprocessing import Preprocessor, at_least_3d
 from ddsp.spectral_ops import F0_RANGE
 from ddsp.core import resample, hz_to_midi, oscillator_bank
 
+F0_SUB_RANGE = 1000 # 1000 Mel = 1000 Hz, which is a lot higher than most f0 / n_cyl
+
+def hz_to_mel(f):
+    return 1127 * tf.math.log(1 + f/700)
+
 class F0Preprocessor(Preprocessor):
     '''
     Preprocessor for f0 feature. Scales f0 envelope by converting to MIDI scale
@@ -50,5 +55,46 @@ class F0Preprocessor(Preprocessor):
                 features["f0_scaled"] = features["osc"][:,:,tf.newaxis]
         else:
             raise ValueError("%s is not a valid value for feature_domain." % self.feature_domain)
+
+        return features
+
+class OscF0Preprocessor(Preprocessor):
+    '''
+    Preprocessor for f0 and osc features. Scales f0 envelope by converting to
+    MIDI scale and normalizing to [0, 1].
+    '''
+    def __init__(self, time_steps=None, denom=1., rate=None):
+        if time_steps is None:
+            raise ValueError("time_steps cannot be None.")
+        super().__init__()
+        self.time_steps = time_steps
+        self.denom = denom
+        self.rate = rate
+
+    def __call__(self, features, training=True):
+        super().__call__(features, training)
+        return self._default_processing(features)
+
+    def _default_processing(self, features):
+        '''Always resample to time_steps and scale f0 signal.'''
+        for k in ["f0", "osc"]:
+            if features.get(k, None) is not None:
+                features[k] = at_least_3d(features[k])
+                features[k] = resample(features[k], n_timesteps=self.time_steps)
+        
+        # Divide by denom (e.g. number of cylinders in engine to produce subharmonics)
+        features["f0_sub"] = features["f0"] / self.denom
+        
+        # Prepare decoder network inputs
+        features["f0_scaled"] = hz_to_midi(features["f0"]) / F0_RANGE
+        features["f0_sub_scaled"] = hz_to_mel(features["f0_sub"]) / F0_SUB_RANGE
+        if features.get("osc", None) is not None:
+            features["osc_scaled"] = .5 + .5*features["osc"]
+        else:
+            amplitudes = tf.ones(tf.shape(features["f0"]))
+            features["osc"] = oscillator_bank(features["f0"],
+                                              amplitudes,
+                                              sample_rate=self.rate)[:,:,tf.newaxis]
+            features["osc_scaled"] = .5 + .5*features["osc"]
 
         return features
