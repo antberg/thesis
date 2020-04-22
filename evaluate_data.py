b005c2af838cac46252a9a81f9117289e85ea411
Anton Lundberg <antlundb@kth.se>
2020-04-22 09:41:55 +0200

    add function to create input plots and spectrograms for individual examples in a dataset

--- evaluate_data.py
+++ evaluate_data.py
@@ -4,13 +4,28 @@ Script for evaluating data.
 import os
 from absl import app, flags, logging
 
-from data_provider import TFRecordProvider
+from data.data_provider import TFRecordProvider
 from evaluation.data import DataEvaluator
 
+# Define flags
 FLAGS = flags.FLAGS
-flags.DEFINE_string("data_dir", None, "Path to directory of TFRecord dataset.")
-flags.DEFINE_bool("f0_hist", False, "Whether to plot f0 histograms.")
+flags.DEFINE_string("data_dir", os.path.join(".", "data", "tfrecord", "ford_final"),
+                    "Path to directory of TFRecord dataset.")
+flags.DEFINE_bool("show", False,
+                  "Whether to show the plots.")
+flags.DEFINE_bool("f0_hist", False,
+                  "Whether to plot f0 histograms.")
+flags.DEFINE_bool("ex_figures", False,
+                  "Whether to produce figures illustrating individual examples " + \
+                  "(Spectrograms and input plots).")
+flags.DEFINE_list("ex_ids_train", [1, 127, 78],
+                  "Training examples to illustrate (if ex_figures is True).")
+flags.DEFINE_list("ex_ids_test", [1, 42, 49],
+                  "Test examples to illustrate (if ex_figures is True).")
+flags.DEFINE_list("ex_input_keys", ["f0", "phase_sub_sync"],
+                  "Input keys to show in example illustrations (if ex_figures is True).")
 
+# Define global constants
 FIG_BASE_DIR = os.path.join(".", "evaluation", "fig")
 FIG_EXTENSION = "pdf"
 
@@ -27,7 +42,9 @@ def main(argv):
                  "f0_diff_hist_xlim": None,
                  "f0_diff_hist_ylim": None,
                  "f0_diff_hist_bins": None,
-                 "f0_diff_hist_yscale": "log"}
+                 "f0_diff_hist_yscale": "log",
+                 "ex_ids": {"train": FLAGS.ex_ids_train,
+                            "test": FLAGS.ex_ids_test}}
 
     for split in DataEvaluator.SPLITS:
         # Load data and initialize data evaluator
@@ -65,7 +82,7 @@ def main(argv):
                     f0_hist_data,
                     input_key=input_key,
                     norm=True,
-                    show=False,
+                    show=FLAGS.show,
                     save_path=fig_path,
                     split=split,
                     xlabel="fundamental frequency $f_0$ [Hz]",
@@ -75,6 +92,36 @@ def main(argv):
                 if constants[input_key + "_hist_xlim"] is None:
                     constants[input_key + "_hist_xlim"] = xlim
                     constants[input_key + "_hist_ylim"] = ylim
+        
+        # Plot Mel spectrograms and input plots
+        if FLAGS.ex_figures and \
+           split not in ["all", "valid"]: # only use train and test examples
+            # For each example: get data, plot inputs, plot Mel spectrogram
+            for ex_id in constants["ex_ids"][split]:
+                # Get data
+                logging.info("Getting input data data for %s example %d of '%s'..." % \
+                             (split, ex_id, data_evaluator.dataset_name))
+                ex_dict = data_evaluator.get_example_dict(ex_id, FLAGS.ex_input_keys)
+
+                # Input plots
+                fig_path = os.path.join(fig_dir, "ex_inputs_%s_%d.%s" % (split, ex_id, FIG_EXTENSION))
+                logging.info("Plotting inputs for %s example %d of '%s'..." % \
+                             (split, ex_id, data_evaluator.dataset_name))
+                data_evaluator.plot_inputs_from_dict(ex_dict, split, save_path=fig_path, show=FLAGS.show)
+
+                # Spectrograms
+                logging.info("Plotting spectrograms for %s example %d of '%s'..." % \
+                             (split, ex_id, data_evaluator.dataset_name))
+                for spec_type in ["mel", "cqt"]:
+                    fig_path = os.path.join(fig_dir, "ex_%s_%d_%s.%s" % \
+                                            (split, ex_id, spec_type, FIG_EXTENSION))
+                    data_evaluator.plot_spectrogram_from_dict(ex_dict, spec_type=spec_type,
+                                                                       plot_f0=True,
+                                                                       split=split,
+                                                                       save_path=fig_path,
+                                                                       show=FLAGS.show)
+
+    logging.info("Done!")
 
 if __name__ == "__main__":
     app.run(main)
--- evaluation/data.py
+++ evaluation/data.py
@@ -4,9 +4,19 @@ import numpy as np
 from absl import logging
 import matplotlib.pyplot as plt
 import seaborn as sns
+import librosa
+from librosa.display import specshow
 
 class DataEvaluator:
-    SPLITS = ["all", "train", "valid", "test"]
+    SPLITS = ["all", "train", "test", "valid"]
+    INPUT_UNITS = {"f0": "Hz",
+                   "phase": "rad/s",
+                   "phase_sub": "rad/s",
+                   "phase_sub_sync": "rad/s"}
+    LATEX_FROM_KEY = {"f0": "$f_0$",
+                      "phase": "$\phi$",
+                      "phase_sub": "$\phi$",
+                      "phase_sub_sync": "$\phi$"}
 
     '''
     Class for evaluating a dataset.
@@ -15,6 +25,29 @@ class DataEvaluator:
         '''Construct the DataEvaluator.'''
         self.data_provider = data_provider
         self.dataset_name = os.path.basename(data_provider.data_dir)
+        self.input_stats = dict()
+        for input_key in data_provider.input_keys:
+            self.input_stats[input_key] = None
+
+    def get_input_stats(self, input_key, stat=None):
+        '''Get statistics of input.'''
+        if self.input_stats[input_key] is None:
+            self.compute_input_stats(input_key)
+        return self.input_stats[input_key] if stat is None else self.input_stats[input_key][stat]
+
+    def compute_input_stats(self, input_key):
+        '''Compute statistics of input.'''
+        dataset = self.data_provider.get_batch(1, repeats=1)
+        input_list = []
+        for input_tensor in iter(dataset):
+            example_numpy = input_tensor[input_key].numpy().flatten()
+            input_list.append(example_numpy)
+        input_numpy = np.concatenate(input_list)
+        self.input_stats[input_key] = dict()
+        self.input_stats[input_key]["min"] = np.min(input_numpy)
+        self.input_stats[input_key]["max"] = np.max(input_numpy)
+        self.input_stats[input_key]["mean"] = np.mean(input_numpy)
+        self.input_stats[input_key]["std"] = np.min(input_numpy)
     
     def get_f0_hist_dict(self, bins_f0=None, bins_f0_diff=None, verbose=1):
         '''Get histogram data for f0 and f0_diff.'''
@@ -36,11 +69,16 @@ class DataEvaluator:
         data["dataset_secs"] = n_samples * self.data_provider.example_secs
         data["f0"] = np.concatenate(input_list)
         data["f0_diff"] = np.concatenate(input_diff_list)
+        self.input_stats["f0"] = dict(min=np.min(data["f0"]),
+                                      max=np.max(data["f0"]),
+                                      mean=np.mean(data["f0"]),
+                                      std=np.std(data["f0"]))
         if verbose > 0:
+            stats = self.input_stats["f0"]
             logging.info("Dataset %s (split: %s) has %d entries (%.1f seconds)." % \
                          (self.dataset_name, self.data_provider.split, n_samples, data["dataset_secs"]))
-            logging.info("f0 statistics: max = %.2f; min = %.2f; mean = %.2f; std = %.2f" % \
-                         (np.max(data["f0"]), np.min(data["f0"]), np.mean(data["f0"]), np.std(data["f0"])))
+            logging.info("f0 statistics: min = %.2f; max = %.2f; mean = %.2f; std = %.2f" % \
+                         (stats["min"], stats["max"], stats["mean"], stats["std"]))
         
         # Calculate f0 histogram data
         f0_hist_data = self.get_hist_dict_from_numpy(data["f0"], bins_f0, prefix="f0")
@@ -104,6 +142,7 @@ class DataEvaluator:
     
     @staticmethod
     def plot_postprocess(save_path=None, show=True):
+        '''Postprocess current plot.'''
         plt.tight_layout()
         if save_path is not None:
             plt.savefig(save_path)
@@ -112,11 +151,20 @@ class DataEvaluator:
         xlim, ylim = plt.xlim(), plt.ylim()
         plt.close()
         return xlim, ylim
+    
+    @staticmethod
+    def remove_xticks(ax=plt):
+        ax.tick_params(axis="x",          # changes apply to the x-axis
+                       which="both",      # both major and minor ticks are affected
+                       bottom=False,      # ticks along the bottom edge are off
+                       top=False,         # ticks along the top edge are off
+                       labelbottom=False) # labels along the bottom edge are off
 
     @staticmethod
     def plot_hist_from_dict(data, input_key="", norm=False, save_path=None,
                             show=True, split="all", xlabel=None, xlim=None,
                             ylim=None, yscale="linear", **fig_kwargs):
+        '''Plot input histogram from dict.'''
         prefix = "" if input_key is None else input_key + "_"
         suffix = "_norm" if norm else ""
         y = data["%shist_count%s" % (prefix, suffix)]
@@ -134,3 +182,107 @@ class DataEvaluator:
     @staticmethod
     def plot_f0_hist_from_file(file_path):
         raise NotImplementedError
+    
+    def get_example_dict(self, example_id, input_keys="all"):
+        '''Get data example dict given an example id.'''
+        batch = self.data_provider.get_single_batch(batch_number=example_id)
+        data = dict()
+        data["audio"] = batch["audio"].numpy().flatten()
+        data["audio_rate"] = self.data_provider.audio_rate
+        data["input_rate"] = self.data_provider.input_rate
+        data["inputs"] = dict()
+        data["input_stats"] = dict()
+        for input_key in self.data_provider.input_keys:
+            if input_keys == "all" or input_key in input_keys:
+                data["inputs"][input_key] = batch[input_key].numpy().flatten()
+                data["input_stats"][input_key] = self.get_input_stats(input_key)
+        return data
+    
+    @staticmethod
+    def plot_inputs_from_dict(data, split="all", save_path=None, show=True, **fig_kwargs):
+        '''Plot inputs for a given example.'''
+        if fig_kwargs.get("figsize", None) is None:
+            fig_kwargs["figsize"] = (4, 4)
+        sns.set(palette="colorblind")
+        color = list(sns.color_palette())[DataEvaluator.SPLITS.index(split)]
+        n_inputs = len(data["inputs"])
+        n_subplots = 1 + n_inputs
+        _, axes = plt.subplots(n_subplots, 1, **fig_kwargs)
+        axes_iter = iter(axes)
+        ax = next(axes_iter)
+        t = np.arange(0.0, len(data["audio"]))/data["audio_rate"]
+        ax.plot(t, data["audio"], color=color)
+        ax.set_ylim((-1, 1))
+        ax.set_ylabel("audio")
+        DataEvaluator.remove_xticks(ax)
+        for i, keyval in enumerate(data["inputs"].items()):
+            key, values = keyval
+            ax = next(axes_iter)
+            t = np.arange(0.0, len(values))/data["input_rate"]
+            ax.plot(t, values, color=color)
+            ylim_min = np.floor(data["input_stats"][key]["min"])
+            ylim_max = np.ceil(data["input_stats"][key]["max"])
+            ax.set_ylim((ylim_min, ylim_max))
+            unit = DataEvaluator.INPUT_UNITS.get(key)
+            unit = " [%s]" % unit if unit else ""
+            tex_label = DataEvaluator.LATEX_FROM_KEY.get(key)
+            tex_label = tex_label if tex_label else key
+            ylabel = tex_label + unit
+            ax.set_ylabel(ylabel)
+            if i + 1 < n_inputs:
+                DataEvaluator.remove_xticks(ax)
+        ax.set_xlabel("time [s]")
+        return DataEvaluator.plot_postprocess(save_path, show)
+    
+    @staticmethod
+    def plot_spectrogram_from_dict(data, spec_type="mel", split="all", plot_f0=False, save_path=None, show=True, **fig_kwargs):
+        '''Plot spectrogram for a given example.'''
+        if fig_kwargs.get("figsize", None) is None:
+            fig_kwargs["figsize"] = (4, 6)
+        if spec_type == "mel":
+            S_dB = DataEvaluator.get_mel_spectrogram_from_dict(data)
+            specshow_kw = dict(y_axis="mel")
+        elif spec_type == "cqt":
+            fmin = 20.0
+            bins_per_octave = 16
+            n_octaves = int(np.log(data["audio_rate"] / 2 / fmin) / np.log(2))
+            n_bins = bins_per_octave * n_octaves
+            cqt_kw = dict(fmin=fmin, n_bins=n_bins, bins_per_octave=bins_per_octave)
+            S_dB = DataEvaluator.get_cqt_spectrogram_from_dict(data, **cqt_kw)
+            specshow_kw = cqt_kw
+            specshow_kw.update(dict(y_axis="cqt_hz"))
+            specshow_kw.pop("n_bins")
+        else:
+            raise ValueError("%s is not a valid spectrogram type.")
+        sns.set(palette="colorblind")
+        color = list(sns.color_palette())[DataEvaluator.SPLITS.index(split)]
+        _, ax = plt.subplots(1, 1, **fig_kwargs)
+        specshow(S_dB, x_axis="time", sr=data["audio_rate"], fmax=data["audio_rate"]/2, ax=ax, **specshow_kw)
+        if plot_f0:
+            f0 = data["inputs"]["f0"]
+            t = np.arange(0.0, len(f0))/data["input_rate"]
+            ax.plot(t, f0, "--", color=color, label="$f_0$")
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower right")
+        ax.set_ylabel("frequency [Hz]")
+        ax.set_xlabel("time [s]")
+        return DataEvaluator.plot_postprocess(save_path, show)
+
+    @staticmethod
+    def get_mel_spectrogram_from_dict(data, n_fft=4092, n_mels=512):
+        '''Get Mel spectrogram from dict.'''
+        audio = np.asfortranarray(data["audio"])
+        S = librosa.feature.melspectrogram(y=audio,
+                                           sr=data["audio_rate"],
+                                           n_fft=n_fft,
+                                           n_mels=n_mels,
+                                           fmax=data["audio_rate"]/2)
+        S_dB = librosa.power_to_db(S, ref=np.max)
+        return S_dB
+
+    @staticmethod
+    def get_cqt_spectrogram_from_dict(data, **kwargs):
+        '''Get constant-Q transform spectrogram from dict.'''
+        audio = np.asfortranarray(data["audio"])
+        S = np.abs(librosa.cqt(audio, sr=data["audio_rate"], **kwargs))
+        S_dB = librosa.amplitude_to_db(S, ref=np.max)
+        return S_dB
